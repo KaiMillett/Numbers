05a57f1c72547ff304be4600f119a9f8b1861414
Kai Millett <RC0041917@student.riversidecollege.ac.uk>
2021-04-19 15:22:33 +0100

    Numbers

--- Numbers/Form1.Designer.cs
+++ Numbers/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(168, 252);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
